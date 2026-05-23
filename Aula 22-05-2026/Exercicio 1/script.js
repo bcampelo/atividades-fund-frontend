673b8ae6f37847baa916fa812c80491a8b324df9
bcampelo <brenoccfilho@hotmail.com>
2026-05-22 20:04:27 -0500

    feat: Aula do dia 22-05-2026, sobre JavaScript

---
+++ Aula 22-05-2026/Exercicio 1/script.js
@@ -0,0 +1,32 @@
+console.log("Olá, mundo!");
+
+// console
+
+console.error("Isso é um erro!");
+console.warn("Isso é um aviso!");
+
+// variáveis
+var x = 10;
+
+// variáveis modernas
+let y = 15;
+const z = 20;
+
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(typeof x);
+
+linguagens = ["JavaScript", "Python", "Java"];
+console.log(linguagens);
+console.log(typeof linguagens);
+
+// -----------------------------------------
+
+const user = {
+    nome: "João",
+    idade: 30,
+    email: "joao@gmail.com",
+}
+console.log(user);
+console.log(typeof user);
